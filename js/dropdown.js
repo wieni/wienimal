9d94ea33be3b5c61412248d1a57810ac9214ef68
sqndr <me@sqndr.com>
2017-01-05 14:40:13 +0100

    Continue fixing views_ui

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,16 @@
-(function($) {
-    var $button = $('.dropdown-toggle');
+(function ($, Drupal) {
 
-    $button.on('click', function() {
-        $(this).closest('.btn-group').addClass('js-remain');
-        $button.closest('.btn-group:not(.js-remain)').removeClass('open');
-        $(this).closest('.btn-group').toggleClass('open');
-        $(this).closest('.btn-group').removeClass('js-remain');
-    });
+    Drupal.behaviors.dropdown = {
+        attach: function(context) {
+            var $button = $('.dropdown-toggle', context);
 
-})(jQuery);
+            $button.on('click', function () {
+                $(this).closest('.btn-group').addClass('js-remain');
+                $button.closest('.btn-group:not(.js-remain)').removeClass('open');
+                $(this).closest('.btn-group').toggleClass('open');
+                $(this).closest('.btn-group').removeClass('js-remain');
+            });
+        }
+    };
+
+})(jQuery, Drupal);
