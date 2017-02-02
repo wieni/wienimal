39413bb81bf50027f8747a245c2f0439cb23f9aa
sqndr <me@sqndr.com>
2017-02-02 14:55:56 +0100

    🐷  Add breadcrumbs … woop woop

--- theme-settings.php
+++ theme-settings.php
@@ -13,7 +13,7 @@ function wienimal_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\For
           '#attributes' => ['class' => ['styles']],
           '#title' => t('Toolbar settings'),
           '#weight' => -899,
-          '#group' => 'omega',
+          '#group' => 'toolbar',
           '#open' => TRUE,
           '#tree' => TRUE,
         ];
@@ -31,9 +31,29 @@ function wienimal_form_system_theme_settings_alter(&$form, \Drupal\Core\Form\For
 
         $form['#submit'][] = 'wienimal_form_settings_submit';
     }
+
+    $form['options'] = [
+      '#type' => 'details',
+      '#attributes' => ['class' => ['styles']],
+      '#title' => t('Wienimal options'),
+      '#weight' => -899,
+      '#group' => 'wienimal_options',
+      '#open' => TRUE,
+      '#tree' => TRUE,
+    ];
+
+    $form['options']['breadcrumb_show'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Show breadcrumbs'),
+      '#default_value' => \Drupal::config('wienimal.settings')->get('breadcrumbs.show'),
+      '#description' => t('The theme for the toolbar'),
+    ];
 }
 
 function wienimal_form_settings_submit(&$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    $values = $form_state->getValue('admin_toolbar');
-    \Drupal::service('config.factory')->getEditable('wienimal.settings')->set('toolbar.theme', $values['toolbar_theme'])->save();
+    $toolbar_values = $form_state->getValue('admin_toolbar');
+    \Drupal::service('config.factory')->getEditable('wienimal.settings')->set('toolbar.theme', $toolbar_values['toolbar_theme'])->save();
+
+    $breadcrumbs_values = $form_state->getValue('options');
+    \Drupal::service('config.factory')->getEditable('wienimal.settings')->set('breadcrumbs.show', $breadcrumbs_values['breadcrumb_show'])->save();
 }
