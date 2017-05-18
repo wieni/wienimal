4f6f4d9be21a1c54f29509549739bddb792a8801
Sander Tirez <me@sqndr.com>
2017-05-18 20:30:02 +0200

    Remove console.log from @dieter

--- js/maxlength.behaviors.js
+++ js/maxlength.behaviors.js
@@ -2,7 +2,6 @@
 
   Drupal.behaviors.wienimal_maxlength = {
       attach: function (context) {
-          console.log($('.counter').length);
           $(context).find('.maxlength').once('wienimal_maxlength').each(function () {
               $(this)
                   .add('.counter', $(this).parent())
