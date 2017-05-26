5de0af7a762636892b0a6056777f5be15f355e06
Kobe Aerts <kobe.aerts@gmail.com>
2017-05-26 16:07:08 +0200

    Admin table + select 2 for tags fields

--- js/select2.behaviors.js
+++ js/select2.behaviors.js
@@ -3,6 +3,16 @@
 
     Drupal.behaviors.select2 = {
         attach: function (context) {
+            $('.form-type-select-autocreate select', context).once('select2').each(function () {
+                console.log('hihi');
+                
+                $(this).select2({
+                    minimumResultsForSearch: 10,
+                    width: 'auto',
+                    tags: true,
+                });
+            });
+
             $('select', context).once('select2').each(function () {
                 $(this).select2({
                     minimumResultsForSearch: 10,
