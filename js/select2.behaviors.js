1bd43d559d654fd10bba3ea90f2c63e28a689471
Sander Tirez <me@sqndr.com>
2017-05-31 10:09:38 +0200

    Select HIHI console.log … 😕

--- js/select2.behaviors.js
+++ js/select2.behaviors.js
@@ -4,8 +4,6 @@
     Drupal.behaviors.select2 = {
         attach: function (context) {
             $('.form-type-select-autocreate select', context).once('select2').each(function () {
-                console.log('hihi');
-
                 $(this).select2({
                     minimumResultsForSearch: 10,
                     width: 'auto',
