48ab8fab103318d0571a120148d3d1e933dd4466
Kobe Aerts <kobe.aerts@gmail.com>
2017-06-14 11:51:01 +0200

    Select 2: remove auto create fields

--- js/select2.behaviors.js
+++ js/select2.behaviors.js
@@ -3,14 +3,6 @@
 
     Drupal.behaviors.select2 = {
         attach: function (context) {
-            $('.form-type-select-autocreate select', context).once('select2').each(function () {
-                $(this).select2({
-                    minimumResultsForSearch: 10,
-                    width: 'auto',
-                    tags: true,
-                });
-            });
-
             $('select', context).once('select2').each(function () {
                 $(this).select2({
                     minimumResultsForSearch: 10,
